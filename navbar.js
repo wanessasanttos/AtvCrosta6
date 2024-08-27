0f648cf10801bbe9e924e1883d64e10719ee525a
Gabriel <bieljvsa@gmail.com>
2024-08-27 15:11:11 -0300

    refactor: refaz a navbar com icons e js

--- navbar.js
+++ navbar.js
@@ -1,17 +1,13 @@
-const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu(event) {
-  if (event.type === 'touchstart') event.preventDefault();
-  const nav = document.getElementById('nav');
-  nav.classList.toggle('active');
-  const active = nav.classList.contains('active');
-  event.currentTarget.setAttribute('aria-expanded', active);
-  if (active) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-  } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-  }
+function clickMenu(){
+    if(itens.style.display == 'block'){
+      itens.style.display = 'none'
+      const close = document.getElementById("burguer")
+      close.innerHTML = "menu"
+    }
+    else{
+      itens.style.display = 'block'
+      const close = document.getElementById("burguer")
+      close.innerHTML = "close"
+    }
 }
-
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
